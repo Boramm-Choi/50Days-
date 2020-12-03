85057d714d943f9aa4b436aa63198751f6d7e361
Boramm-Choi <73491757+Boramm-Choi@users.noreply.github.com>
2020-12-03 15:33:01 -0500

    Day2. Progress Steps updated

--- Day2.Progress Steps/script.js
+++ Day2.Progress Steps/script.js
@@ -1,44 +1,49 @@
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-const circles = document.getElementById('.circle')
+const circles = document.querySelectorAll('.circle')
 
-let currentActive =1
+let currentActive = 1
 
-next.addEventListener('click', () =>{
-currentActive++
-if(currentActive > circles.length){
-    currentActive=circles.length
-}
-update()
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
 })
 
-prev.addEventListener('click', () =>{
-    currentActive++
-    if(currentActive < 1){
-        currentActive= 1
-    }
-    update()
-    })
+prev.addEventListener('click', () => {
+    currentActive--
 
-function update(){
-    circles.forEach((circle,idx)=>{
-        if(idx < currentActive){
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
             circle.classList.add('active')
-        }else{
+        } else {
             circle.classList.remove('active')
         }
     })
 
     const actives = document.querySelectorAll('.active')
 
-    progress.style.width= (actives.length-1)/ (circles.length-1)*100+'%'
-    if(currentActive===1){
-        prev.disabled=true
-    }else if(currentActive===circles.length){
-        next.disabled=true
-    }else{
-        prev.disabled=false
-        next.disabled=false
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
     }
-}
+} 
