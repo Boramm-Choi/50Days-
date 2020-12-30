658767c301ec511c231800c8c8c3719e8f3b2fb2
Boramm-Choi <73491757+Boramm-Choi@users.noreply.github.com>
2020-12-29 20:25:05 -0500

    Day17.Movie-App
    adding API created movie app

---
+++ Day17. Movie-App/script.js
@@ -0,0 +1,78 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bbedf589d4711986afe498635061512c&page=1'
+const IMG_PATH= 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API= 'https://api.themoviedb.org/3/discover/movie?api_key=bbedf589d4711986afe498635061512c&query="'
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const serach = document.getElementById('search')
+// get initial movies
+
+getMovies(API_URL)
+
+async function getMovies(url){
+    const res = await fetch(url)
+    const data= await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const{ title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+        main.appendChild(movieEl)
+
+    })
+}
+
+function getClassByRate(vote){
+    if(vote >= 8) {
+        return 'green'
+    }else if (vote >= 5){
+        return ' orange'
+    }else {
+        return 'red'
+    }
+}
+
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+
+    const serachTerm = search.value
+
+    if(serachTerm && serachTerm !== ''){
+        getMovies(SEARCH_API + serachTerm)
+        
+        search.value = ''
+    }else{
+        window.location.reload()
+    }
+
+})
+
+const search = document.querySelector('.search')
+const searchbar = document.querySelector('.searchbar')
+const input = document.querySelector('.input')
+
+searchbar.addEventListener('click', () =>{
+    search.classList.toggle('active')
+    input.focus()
+})
