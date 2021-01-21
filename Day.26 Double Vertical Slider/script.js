ffcf39fcf4bdffabeab4554f6f85f27b438c5214
Boramm-Choi <73491757+Boramm-Choi@users.noreply.github.com>
2021-01-21 16:47:04 -0500

    Day26. Double Vertical Slider

---
+++ Day.26 Double Vertical Slider/script.js
@@ -0,0 +1,31 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+
+let activeSlideIndex = 0
+
+slideLeft.getElementsByClassName.top = `-${(slidesLength -1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSldie ('up'))
+downButton.addEventListener('click', () => changeSldie ('down'))
+
+const changeSldie = (directon) => {
+    const slideHeight = sliderContainer.clientHeight
+    if (directon ==='up'){
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength -1){
+            activeSlideIndex = 0
+        }
+    } else if (directon === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesLength - 1
+    }
+
+    slideRight.style.transform = `translateY (-${activeSlideIndex * sliderHeight} px)`
+    slideLeft.style.transform = `translateY (${activeSlideIndex * sliderHeight} px)`
+}
